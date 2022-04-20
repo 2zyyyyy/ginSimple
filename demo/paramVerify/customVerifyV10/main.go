64f0f5233ffc8e63105621eb61b5119a3483276e
2zyyyyy <android4google@163.com>
2022-04-20 22:18:51 +0800

    add:多语言翻译验证

--- demo/paramVerify/customVerifyV10/main.go
+++ demo/paramVerify/customVerifyV10/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
 	"reflect"
 	"strings"
 )
@@ -17,11 +20,6 @@ type User struct {
 
 func main() {
 	validate := validator.New()
-	// 初始化翻译器
-	//trans, err := trans.NewTrans()
-	//if err != nil {
-	//	return
-	//}
 	user := &User{
 		ID:     1001,
 		Name:   "wanli",
@@ -29,6 +27,12 @@ func main() {
 		Age:    10,
 		Email:  "golang@google.gamil.com",
 	}
+	// 错误信息翻译成中文
+	zh := zh.New()
+	uni := ut.New(zh)
+	trans, _ := uni.GetTranslator("zh")
+	_ = en_translations.RegisterDefaultTranslations(validate, trans)
+
 	// 注册一个函数，获取结构体字段的备用名称
 	validate.RegisterTagNameFunc(func(field reflect.StructField) string {
 		name := strings.SplitN(field.Tag.Get("json"), ",", 2)[0]
