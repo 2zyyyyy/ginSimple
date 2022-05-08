7aea479349e009f1317551641e5efd76843f6ffc
2zyyyyy <android4google@163.com>
2022-05-08 23:46:53 +0800

    add:gin使用zap日志,完成zap中间件GinLogger.(50%)

--- demo/zap/ginZap/logger/logger.go
+++ demo/zap/ginZap/logger/logger.go
@@ -1,1 +1,72 @@
 package logger
+
+import (
+	"ginSimple/demo/zap/ginZap/config"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/natefinch/lumberjack"
+
+	"go.uber.org/zap/zapcore"
+
+	"go.uber.org/zap"
+)
+
+var logger *zap.Logger
+
+func InitLogger(cfg *config.LogConfig) (err error) {
+	writeSyncer := getLogWrite(cfg.Filename, cfg.MaxAge, cfg.MaxSize, cfg.MaxBackups)
+	encoder := getEncoder()
+	var level = new(zapcore.Level)
+	err = level.UnmarshalText([]byte(cfg.Level))
+	if err != nil {
+		return
+	}
+	core := zapcore.NewCore(encoder, writeSyncer, level)
+	logger = zap.New(core, zap.AddCaller())
+	zap.ReplaceGlobals(logger) // 替换zap包中全局的logger实例 后续在其他包中只需要使用zap.l()即可
+	return
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderCfg.TimeKey = "time"
+	encoderCfg.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderCfg.EncodeDuration = zapcore.SecondsDurationEncoder
+	encoderCfg.EncodeCaller = zapcore.ShortCallerEncoder
+	return zapcore.NewJSONEncoder(encoderCfg)
+}
+
+func getLogWrite(filename string, maxSize, maxBackups, maxAge int) zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   filename,
+		MaxSize:    maxSize,
+		MaxAge:     maxAge,
+		MaxBackups: maxBackups,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
+
+// GinLogger 接收gin框架默认的日志
+func GinLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+
+		cost := time.Since(start)
+		logger.Info(
+			path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("error", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost))
+	}
+}
