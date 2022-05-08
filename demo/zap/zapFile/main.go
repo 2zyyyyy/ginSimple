f926f2041b28c03e088e77865af1ad9028af7011
2zyyyyy <android4google@163.com>
2022-05-08 19:57:50 +0800

    add:使用lumberJackLogger实现日志切割（根据文件大小）

--- demo/zap/zapFile/main.go
+++ demo/zap/zapFile/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"net/http"
-	"os"
 )
 
 // 将日志写入文件
@@ -28,20 +27,35 @@ func InitLogger() {
 	encoder := getEncoder()
 	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
 
-	logger := zap.New(core)
+	logger := zap.New(core, zap.AddCaller()) // 添加将调用函数信息记录到日志中的功能
 	sugarLog = logger.Sugar()
 }
 
 func getEncoder() zapcore.Encoder {
-	return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	// 修改时间编码器  在日志文件中使用大写字母记录日志级别
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	//return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	// 将编码器从JSON Encoder更改为普通Encoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
 }
 
 func getLogWrite() zapcore.WriteSyncer {
-	file, err := os.Create("./jsonEncoder.log")
-	if err != nil {
-		fmt.Println("create file failed, err:", err)
+	//file, err := os.Create("./consoleEncoder.log")
+	//if err != nil {
+	//	fmt.Println("create file failed, err:", err)
+	//}
+
+	// zap本身不支持文件切割 需要使用Lumberjack进行日志切割归档
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./consoleEncoder.log", // 日志文件位置
+		MaxSize:    10,                     // 切割之前 日志文件最大大小（mb）
+		MaxAge:     30,                     // 保留历史文件的最大天数
+		MaxBackups: 5,                      // 保留历史文件的最大个数
+		Compress:   false,                  // 是否压缩/归档历史文件
 	}
-	return zapcore.AddSync(file)
+	return zapcore.AddSync(lumberJackLogger)
 }
 
 // zap Sugared Logger
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.19.0 // indirect
 	github.com/ugorji/go v1.2.7 // indirect
